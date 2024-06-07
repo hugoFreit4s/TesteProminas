b56cd2f249f4f220833b16b8e81d1c78de8dd43e
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-06-07 14:03:10 -0300

    Modificações e novas imagens

--- prominas/components/social.js
+++ prominas/components/social.js
@@ -1,10 +1,21 @@
 const Social = () => {
     return (
-        <div w-full h-full>
-            <h1 className="text-[20px] font-bold" style={{ fontFamily: 'Poppins, sans-serif' }}>Siga nossa faculdade:</h1>
-            <img className="h-[50px]" src='/images/facebooklogopng.png' />
-            <img className="h-[80px]" src='/images/instagramlogopng.png' />
-            <img className="h-[80px]" src='/images/youtubelogoiconpng.png' />
+        <div className="w-full grid" style={{ gridTemplateColumns: '1fr 1fr 1fr' }}>
+            <div style={{ gridColumn: '1' }}>
+                <h1 className="text-[20px] font-bold" style={{ fontFamily: 'Poppins, sans-serif', gridColumn: '1' }}>Siga nossa faculdade:</h1>
+                <img className="h-[50px]" src='/images/facebooklogopng.png' />
+                <img className="h-[80px]" src='/images/instagramlogopng.png' />
+                <img className="h-[80px]" src='/images/youtubelogoiconpng.png' />
+            </div>
+            <div style={{ gridColumn: '2' }}>
+                <h1 className="text-[20px] font-bold" style={{ fontFamily: 'Poppins, sans-serif', gridColumn: '2' }}>Siga nossa faculdade:</h1>
+                <img className="h-[80px]" src='/images/baixarappgoogleplay.png' style={{ borderRadius: '20px' }} />
+                <img className="h-[80px]" src='/images/baixarappstore.png' style={{ borderRadius: '20px' }} />
+            </div>
+            <div style={{ gridColumn: '3' }}>
+                <h1 className="text-[20px] font-bold" style={{ fontFamily: 'Poppins, sans-serif' }}>Consulte aqui o cadastro da Instituição no Sistema e-MEC</h1>
+                <img className="h-[200px]" src='/images/cadastro-instituicao.png' />
+            </div>
         </div>
     )
 }
